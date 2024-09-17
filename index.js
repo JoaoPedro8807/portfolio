e75dca0a20443dd8befb37a07e00f54396bb6164
João Pedro <joaogood@outlook.com>
2024-09-17 17:41:13 -0300

    att portfolio

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 
 const observer = new IntersectionObserver( props =>{
     props.forEach( prop =>{
+        console.log(prop.target)
         if(prop.isIntersecting){
             prop.target.classList.add('show')
+        }else{
+            if(!prop.target.classList.contains('intro-container')){
+                prop.target.classList.remove('show')
+            }
         }
     } )
 } ) 
 
 const sections_elements = document.querySelectorAll('.section')
 const jobs_cards = document.querySelectorAll('.job-card-container')
+const cards = document.querySelectorAll('.card')
+
 
 
 sections_elements.forEach(e => observer.observe(e))
 jobs_cards.forEach(e => observer.observe(e))
+cards.forEach(e => observer.observe(e))
